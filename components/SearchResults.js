fc5e4414e1e7e33e3e025aaf5cc111e2eba2f3e4
Ronak <ronakpatil77777@gmail.com>
2023-03-01 11:37:38 +0530

    check

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -4,8 +4,7 @@ function SearchResults({results}) {
     <div className='mx-auto w-full pl-3 px-3 
     sm:pl-[5%] md:pl-[14%] lg:pl-52'>
       <p className="text-gray-600 text-md mb-5 mt-3">
-        About {results.searchInformation?.formattedTotalResults} 
-        results({results.searchInformation?.formattedSearchTime} seconds)
+        About {results.searchInformation?.formattedTotalResults} results({results.searchInformation?.formattedSearchTime} seconds)
       </p>
 
       {/*results*/}
--- keys.js
+++ keys.js
@@ -1,4 +1,11 @@
-//api key(allows to use google api)(key that allows us to use google services) - https://developers.google.com/custom-search/v1/using_rest
-CONTEXT_KEY = "AIzaSyD-V-vZr1sh-Qt0kDRJVo-cwCQrrg1O110";
-//context key(create a context by visiting this link) - https://cse.google.com/cse/create/new (you can include cerain sites, you can create custom searhes on google which will only prioririze cerain websites)
-API_KEY = "f59d931c0adb345c0";
+// API Key - https://developers.google.com/custom-search/v1/using_rest
+
+export const API_KEY = "AIzaSyD-V-vZr1sh-Qt0kDRJVo-cwCQrrg1O110";
+
+// Context Key - https://cse.google.com/cse/create/new
+
+{
+  /* <script async src="https://cse.google.com/cse.js?cx=ccac24589e9d382a7"></script> */
+}
+
+export const CONTEXT_KEY = "f59d931c0adb345c0";
